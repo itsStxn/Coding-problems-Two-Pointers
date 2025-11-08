abd1edd6f717f5475d6b40d238ec32e4a9da1101
Stanley Sandjo <r0878901@student.thomasmore.be>
2025-11-08 01:35:39 +0100

    Unsolved - C# - Trapping Rain Water

--- Hard/Trapping Rain Water/C#/Solution.cs
+++ Hard/Trapping Rain Water/C#/Solution.cs
@@ -3,5 +3,46 @@ using System;
 namespace Trapping_Rain_Water;
 
 public class Solution {
+	public int Trap(int[] height) {
+		int n = height.Length;
+		if (n-- < 3) return 0;
 
+		int left = 0, leftPeak = 0, 
+		right = n, rightPeak = n,
+
+		trappedWater = 0,
+		rightWater = 0,
+		leftWater = 0;
+
+		while (++left <= --right) {
+			if (height[left] < height[leftPeak]) {
+				leftWater += height[leftPeak] - height[left];
+			}
+			else {
+				trappedWater += leftWater;
+				leftPeak = left;
+				leftWater = 0;
+			}
+
+			if (left == right) continue;
+
+			if (height[right] < height[rightPeak]) {
+				rightWater += height[rightPeak] - height[right];
+			}
+			else {
+				trappedWater += rightWater;
+				rightPeak = right;
+				rightWater = 0;
+			}
+		}
+
+		int w = rightPeak - leftPeak - 1,
+		h = Math.Min(height[rightPeak], height[leftPeak]);
+		
+		while (++leftPeak < rightPeak) {
+			trappedWater -= Math.Min(height[leftPeak], h);
+		}
+
+		return trappedWater + w * h;
+	}
 }

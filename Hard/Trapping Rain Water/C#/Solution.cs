19d726cbe9c3667c190d60d42f69913ce1c73e49
Stanley Ivan Sandjo <heboivan@gmail.com>
2026-02-07 22:05:21 +0100

    Minor code variable change - Solved - C# - Trapping Rain Water

--- Hard/Trapping Rain Water/C#/Solution.cs
+++ Hard/Trapping Rain Water/C#/Solution.cs
@@ -4,24 +4,26 @@ namespace Trapping_Rain_Water;
 
 public class Solution {
 	public int Trap(int[] height) {
-		int n = height.Length;
-		if (n-- < 3) return 0;
+		int n = height.Length - 1;
+		if (n <= 1) return 0;
 
 		var (l, lMax) = (0, 0); 
 		var (r, rMax) = (n, 0); 
-		int trappedWater = 0;
+		int trapped = 0;
 
 		while (l < r) {
 			if (height[l] < height[r]) {
 				lMax = Math.Max(lMax, height[l]);
-				trappedWater += lMax - height[l++];
+				trapped += lMax - height[l];
+				l++;
 			}
 			else {
 				rMax = Math.Max(rMax, height[r]);
-				trappedWater += rMax - height[r--];
+				trapped += rMax - height[r];
+				r--;
 			}
 		}
 
-		return trappedWater;
+		return trapped;
 	}
 }
